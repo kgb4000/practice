4b5c2356d559b9bde345309eabff9d5f13e96837
kgb <kester4000@gmail.com>
2024-04-10 09:07:50 -0400

    Added Pineapple sea moss gel benefits.

--- app/faqs/page.js
+++ app/faqs/page.js
@@ -20,10 +20,10 @@ const FAQ = () => {
           </h1>
           <div className="my-14 max-w-3xl mx-auto">
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 What is Sea Moss
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Sea moss is a type of red algae found in the ocean. It is
                 revered for its high nutritional content, boasting a rich array
                 of vitamins, minerals, and antioxidants. Sea moss is commonly
@@ -39,47 +39,47 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Where do you get your sea moss from?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Our sea moss is typically sourced from St. Lucia in the
                 Caribbean.
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 What are the benefits of Sea Moss?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Sea moss is renowned for its numerous health benefits,
                 including:
               </p>
               <ul className="ml-8">
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Rich source of essential minerals like iodine, calcium,
                   potassium, and magnesium.
                 </li>
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Supports immune function and promotes overall wellness.
                   Enhances skin health and promotes a youthful complexion.
                 </li>
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Supports skin health and promotes a youthful complexion.
                 </li>
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Supports digestive health and aids in detoxification.
                 </li>
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Provides natural energy and supports a healthy metabolism.
                 </li>
               </ul>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 How long does Sea Moss last?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 When properly stored in a cool, dry place,{' '}
                 <Link
                   href="/categories/dried-sea-moss"
@@ -93,10 +93,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 What is the difference between Sea Moss and Irish Moss?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 There are many different types of sea moss. Irish moss
                 (Chrondrus Crispus), is just one type of sea moss. Other types
                 include Gracilaria, Eucheuma Cottonii, and Kappaphycus
@@ -104,20 +104,20 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Where are you located?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Our company is headquartered in Lanham, Mayland, but we operate
                 primarily online, serving customers nation wide with our premium
                 sea moss supplements.
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Is your sea moss tested for heavy metals?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Yes, ensuring the safety and purity of our sea moss products is
                 of utmost importance to us. We conduct rigorous testing for
                 heavy metals and contaminants to meet the highest quality
@@ -125,10 +125,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 How do I store sea moss gel
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Sea moss gel should be stored in the refrigerator to maintain
                 its freshness and prevent spoilage. It is recommended to use a
                 clean, airtight container to store the gel and consume it within
@@ -136,10 +136,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 What is your refund policy
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 We stand behind the quality of our products. If you are not
                 satisfied with your purchase for any reason, please contact our
                 customer support team within 90 days of receiving your order for
@@ -147,10 +147,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Can children take sea moss?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 While sea moss is generally considered safe for consumption, it
                 is recommended to consult with a healthcare professional before
                 giving sea moss supplements to children, especially infants and
@@ -158,10 +158,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Can pregnant women take sea moss?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Pregnant women should consult with their healthcare provider
                 before incorporating sea moss supplements into their diet. While
                 sea moss offers many nutritional benefits, it's essential to
@@ -170,10 +170,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Are your sea moss supplements vegan-friendly?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Yes, our sea moss supplements are 100% vegan-friendly. We
                 prioritize using plant-based ingredients and do not include any
                 animal-derived components in our formulations, making them
@@ -181,10 +181,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Can sea moss help with weight loss?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 While sea moss offers various health benefits, including
                 supporting metabolism and promoting digestive health, it's
                 essential to remember that it is not a magic solution for weight
@@ -194,19 +194,19 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Do you ship internationally?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 No, we do not ship our sea moss supplements internationally. We
                 only ship to customers in the continental U.S.
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Can sea moss help improve hair health?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Yes, sea moss is often praised for its potential benefits for
                 hair health. It contains essential nutrients like vitamins A, C,
                 and E, as well as minerals like zinc and sulfur, which are known
@@ -216,11 +216,11 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 Are your sea moss supplements free from additives and
                 preservatives?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Yes, we take pride in offering sea moss supplements that are
                 free from unnecessary additives, preservatives, and fillers. We
                 believe in providing our customers with pure, high-quality
@@ -231,10 +231,10 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 How long does it take for orders to ship?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 We strive to process and ship orders promptly. Typically, orders
                 are processed within 1-2 business days from the time of
                 purchase. Once shipped, delivery times vary depending on your
@@ -244,20 +244,20 @@ const FAQ = () => {
               </p>
             </div>
             <div className="my-8">
-              <h2 className="text-2xl lg:text-3xl leading-10 mb-5">
+              <h2 className="text-xl lg:text-2xl leading-10 mb-2">
                 What are your hours of operation?
               </h2>
-              <p className="text-xl mb-4 leading-loose">
+              <p className="lg:text-xl mb-4 leading-loose">
                 Our hours of operation are:
               </p>
               <ul className="ml-8">
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Mondays - Fridays: 9:00am - 5:00pm [EST]
                 </li>
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Saturadys: 9:00am - 3:00pm [EST]
                 </li>
-                <li className="text-xl mb-4 leading-loose list-disc">
+                <li className="lg:text-xl mb-4 leading-loose list-disc">
                   Sundays: CLOSED
                 </li>
               </ul>
